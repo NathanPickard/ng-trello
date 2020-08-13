ac982e9496b6533dfa7ddc297242b28da13fa5b7
Nathan Pickard <nathan.pickard87@gmail.com>
2020-08-13 16:52:17 -0700

    Generated board, list, and card components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { ListComponent } from './list/list.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    ListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
